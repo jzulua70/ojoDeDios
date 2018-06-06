ff983297d48deb6068f78a96f93259270f2e3e3e
jzulua70 <jzulua70@eafit.edu.co>
2018-06-06 16:11:13 -0500

    modificando findSuspect

--- findSuspect.py
+++ findSuspect.py
@@ -6,7 +6,8 @@ import boto3
 from botocore.exceptions import ClientError
 client=boto3.client('rekognition','us-east-1')
 face_cascade = cv.CascadeClassifier('classifiers/haarcascade_frontalface_alt2.xml')
-cap = cv.VideoCapture("../../video/DiegoWalk.mp4")
+#cap = cv.VideoCapture("../../video/DiegoWalk.mp4")
+#cap = cv.VideoCapture(0)
 #cap = cv.VideoCapture("../../video/VID_20180605_151627.mp4")
 #cap = cv.VideoCapture("../../video/Diego.jpg")
 #cap = cv.VideoCapture("assets/5.jpeg")
@@ -36,14 +37,28 @@ def getCompare(img,target):
 cont = 0
 while(True):
 	ret, img = cap.read()
-	img = cv.resize(img, (1080,500)) 
+	#img = cv.resize(img, (1080,500)) 
 
 	if (cont % 10 == 0):
 		source_face,matches = getCompare(img,"../../video/Diego2.jpg")
 		print(source_face)
 		print(matches)
 		if len(source_face) >0:
+			height, width, channels = img.shape
 			print "Source Face ({Confidence}%)".format(**source_face)
+			ptop= int (source_face["BoundingBox"]["Top"] * height)
+			pleft= int(source_face["BoundingBox"]["Left"] * width)
+			pwidth = int(source_face["BoundingBox"]["Width"]*width)
+			pheight = int(source_face["BoundingBox"]["Height"]*height)
+			cv.circle(img,(pleft,ptop), 2, (0,0,255), -1)
+			cv.circle(img,(pleft+pwidth,ptop+pheight), 2, (0,255,0), -1)
+			crop_image = img[ptop:ptop+pwidth,pleft:pleft+pheight]
+			cv.imshow("crop_image",crop_image)
+			print("bounding", "left", pleft, "top", ptop, "width",pwidth, "height",pheight)
+
+		
+
+		
 
 	# one match for each target face
 		for match in matches:
