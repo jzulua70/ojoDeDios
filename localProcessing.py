d556410e24428bbaa82970e9fc31ce2be17ffc9c
jfrincon <juanfer.rincon@gmail.com>
2018-06-07 13:09:06 -0500

    wanted

--- localProcessing.py
+++ localProcessing.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import boto3
 import copy
+import numpy as np
+
 from os import listdir
 from os.path import isfile, join
 
@@ -18,7 +20,7 @@ def getFaces(img):
 
 def showFaces(img, faces):
     for (x,y,w,h) in faces:
-        print (x,y,w,h)
+        #print (x,y,w,h)
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         crop_img = img[y:y+h, x:x+w]
         cv2.imshow('crop',crop_img)
@@ -26,3 +28,40 @@ def showFaces(img, faces):
         #roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
     cv2.imshow('img',img)
+
+def wanted(suspect_path,crop_image,info_path):
+    wanted_path = "wantedTemplate/wanted.jpeg"
+    wanted_image = cv2.imread(wanted_path)
+    lenghtWanted = len(wanted_image[0])
+    heightWanted = len(wanted_image)
+
+    suspect_image = cv2.imread(suspect_path)
+    suspect_image = cv2.resize(suspect_image,( ((lenghtWanted*80)//100) , ((heightWanted*78)//100) ))
+    lenghtSuspect = len(suspect_image[0])
+    heightSuspect = len(suspect_image)
+
+    wanted_image[int(heightWanted*.2):int(heightWanted*.2) + heightSuspect,int(lenghtWanted*.1):int(lenghtWanted*.1) + lenghtSuspect] = suspect_image
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    crop_image = cv2.resize(crop_image,(lenghtWanted,lenghtWanted))
+
+    cv2.putText(crop_image,'Found',(10,len(crop_image)), font, 3,(255,255,255),2,cv2.LINE_AA)
+    faces = np.concatenate((wanted_image, crop_image), axis=0)
+
+    text = np.zeros((len(faces), (len(faces)//2), 3), np.uint8)*255
+    text[:] = (255,255,255)
+
+    with open(info_path) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    pos = 0
+    for line in content:
+        pos+=50
+        cv2.putText(text,line,(10,pos), font, 1,(0,0,0),2,cv2.LINE_AA)
+
+
+    full_image = np.concatenate((faces, text), axis=1)
+
+    cv2.imshow("found",full_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- suspect.py
+++ suspect.py
@@ -6,6 +6,11 @@ from os import listdir
 from os.path import isfile, join
 
 
+crop_image = cv.imread("suspects/suspect1.jpeg")
+info_path = 'info/suspect1.txt'
+local.wanted("suspects/suspect2.jpeg",crop_image,info_path)
+
+'''
 region = 'us-east-1'
 client=boto3.client('rekognition',region)
 
@@ -16,10 +21,11 @@ vid = cv.VideoCapture("../VID_20180605_151627.mp4")
 
 ret, img = vid.read()
 count = 0
+faces = []
 while(ret):
-    img = img[len(img)//2:, 0:len(img[0])//2]
-    faces = []
-    if (count %2 == 0):
+    img = img[(len(img)*3)//8:, 0:len(img[0])*5//8]
+    print(count)
+    if (count %10 == 0):
         faces = local.getFaces(img)
     local.showFaces(img,faces)
     cv.waitKey(1)
@@ -27,3 +33,4 @@ while(ret):
     count +=1
 
 cv.destroyAllWindows()
+'''
