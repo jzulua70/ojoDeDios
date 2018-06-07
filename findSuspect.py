309d353843fb5ebd3fd9c96e73236dd41fdad202
jzulua70 <jzulua70@eafit.edu.co>
2018-06-07 15:53:30 -0500

    Wanted

--- findSuspect.py
+++ findSuspect.py
@@ -12,19 +12,58 @@ face_cascade_profile = cv2.CascadeClassifier('classifiers/haarcascade_profile.xm
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("recognizers/face-trainner.yml")
 
+
+
+
 labels = {}
 with open("pickles/face-labels.pickle", 'rb') as f:
 	og_labels = pickle.load(f)
 	labels = {v:k for k,v in og_labels.items()}
 
 
-cap = cv2.VideoCapture("../../video/DiegoWalk2.mp4")
-#cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture("../../video/DiegoWalk2.mp4")
+cap = cv2.VideoCapture(0)
 #cap = cv2.VideoCapture("../../video/VID_20180605_151627.mp4")
 #cap = cv2.VideoCapture("../../video/Diego.jpg")
 #cap = cv2.VideoCapture("assets/5.jpeg")
 
 #cv2.imshow("target", target)
+
+
+def wanted(suspect_path,crop_image,info_path):
+    wanted_path = "wantedTemplate/wanted.jpeg"
+    wanted_image = cv2.imread(wanted_path)
+    lenghtWanted = len(wanted_image[0])
+    heightWanted = len(wanted_image)
+
+    suspect_image = cv2.imread(suspect_path)
+    suspect_image = cv2.resize(suspect_image,( ((lenghtWanted*80)//100) , ((heightWanted*78)//100) ))
+    lenghtSuspect = len(suspect_image[0])
+    heightSuspect = len(suspect_image)
+
+    wanted_image[int(heightWanted*.2):int(heightWanted*.2) + heightSuspect,int(lenghtWanted*.1):int(lenghtWanted*.1) + lenghtSuspect] = suspect_image
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    crop_image = cv2.resize(crop_image,(lenghtWanted,lenghtWanted))
+
+    cv2.putText(crop_image,'Found',(10,len(crop_image)), font, 3,(255,255,255),2,cv2.LINE_AA)
+    faces = np.concatenate((wanted_image, crop_image), axis=0)
+
+    text = np.zeros((len(faces), (len(faces)//2), 3), np.uint8)
+    text[:] = (255,255,255)
+
+    with open(info_path) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    pos = 0
+    for line in content:
+        pos+=50
+        cv2.putText(text,line,(10,pos), font, 1,(0,0,0),2,cv2.LINE_AA)
+
+
+    full_image = np.concatenate((faces, text), axis=1)
+
+    cv2.imshow("found",full_image)
 
 def getCompare(frame,target):
 	source_face = []
@@ -119,6 +158,7 @@ while(ret):
 		source_face,matches = getCompare(frame,"../../video/Diego2.jpg")
 		print(source_face)
 		print(matches)
+		crop_image=None
 		if len(source_face) >0:
 			height, width, channels = frame.shape
 			print ("Source Face ({Confidence}%)".format(**source_face))
@@ -129,12 +169,12 @@ while(ret):
 			cv2.circle(frame,(pleft,ptop), 2, (0,0,255), -1)
 			cv2.circle(frame,(pleft+pwidth,ptop+pheight), 2, (0,255,0), -1)
 			crop_image = frame[ptop:ptop+pwidth,pleft:pleft+pheight]
-			cv2.imshow("crop_image",crop_image)
+			#cv2.imshow("crop_image",crop_image)
 			print("bounding", "left", pleft, "top", ptop, "width",pwidth, "height",pheight)
 
 	# one match for each target face
-		if len(matches)>1:
-			print("Mechas")
+		if len(matches)>0:
+			wanted("../../video/Diego2.jpg", crop_image,"info/suspect1.txt")
 
 		for match in matches:
 			print ("Target Face ({Confidence}%)".format(**match['Face']))
